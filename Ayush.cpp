3d0fafb6d224781008a6c799ed1b665b20d1fe11
Ayush Gangwar <ayushgangwar3011@gmail.com>
2026-03-17 23:15:59 +0530

    Add Ayush.cpp to print 'Hello, World!'

---
+++ Ayush.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // std::endl adds a newline character
+    return 0; // Signals successful execution to the operating system
+}
